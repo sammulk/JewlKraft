3a834b90e49b89e54f676d804e51f5e76de0751f
ChristenMagi <magi.christen7@gmail.com>
2025-10-23 14:05:47 +0300

    More sprites + Backpack UI for dungeon

--- JewlKraft/Assets/Core/Shop_files/Scripts/EndDay.cs
+++ JewlKraft/Assets/Core/Shop_files/Scripts/EndDay.cs
@@ -3,11 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class EndDay : MonoBehaviour
 {
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         SceneManager.LoadScene("Dungeon_scene");
     }
-    //TODO If walked onto bed teleport to cave
-
 }
